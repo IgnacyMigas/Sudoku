8a82259ab966cc1c7bf73d6b1978583478d0e6b8
Ignacy Migas <gigi.migas@gmail.com>
2017-06-22 22:41:58 +0200

    Add Sudoku main file

---
+++ Sudoku.py
@@ -0,0 +1,10 @@
+import SudokuImageReader
+import Solver
+
+
+if __name__ == "__main__":
+    image = "sudoku.jpg"
+    reader = SudokuImageReader.SudokuImageReader(image)
+    sudoku = Solver.Solver(reader.getBoard())
+    sudoku.solve()
+    print sudoku.solveBoard
