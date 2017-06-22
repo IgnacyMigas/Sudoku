f949705a4b84a9c38360fc14f05fc3b789e1cf77
Ignacy Migas <gigi.migas@gmail.com>
2017-06-22 22:34:34 +0200

    Refactor Board class

--- Board.py
+++ Board.py
@@ -1,6 +1,5 @@
-#!/usr/bin/pyhon2.7.9
-
 import abc
+import numpy
 
 
 class AbstractBoard(object):
@@ -15,41 +14,43 @@ class AbstractBoard(object):
         pass
 
     @abc.abstractmethod
+    def set_board(self, board):
+        pass
+
+    @abc.abstractmethod
     def is_solved(self):
         pass
 
 
 class Board(AbstractBoard):
     def __init__(self):
-        self.board = [None]*9*9
+        self.board = numpy.zeros((9, 9), int)
 
     def put(self, x, y, value):
         if self._check_move_possible(x, y, value):
-            self.board[x+y*9] = value
+            self.board[x][y] = value
             return True
         return False
 
     def get_board(self):
         return self.board
 
+    def set_board(self, board):
+        for i in range(9):
+            for j in range(9):
+                self.board[i][j] = board[i][j]
+
     def is_solved(self):
-        if None in self.board:
+        if 0 not in self.board:
             return True
         return False
 
     def _check_move_possible(self, x, y, value):
-        if self.board[x+y*9] is not None:
+        if self.board[x][y] != 0:
             return False
         for i in range(9):
-            if self.board[x+i*9] == value or self.board[i+y*9] == value:
+            if self.board[x][i] == value or self.board[i][y] == value or self.board[int(x/3)*3+i%3][int(y/3)*3+int(i/3)] == value:
                 return False
-        xtmp = int(x/3)
-        ytmp = int(y/3)
-        print xtmp, ytmp
-        for i in range(3):
-            for j in range(3):
-                if self.board[xtmp*3+i+(ytmp*3+j)*9] == value:
-                    return False
         return True
 
 if __name__ == "__main__":
@@ -66,5 +67,5 @@ if __name__ == "__main__":
     print board.put(2, 0, 3)
     print board.put(2, 1, 3)
     print board.put(2, 2, 3)
-    if not board.is_solved():
-        print board.get_board()
+    print board.get_board()
+
