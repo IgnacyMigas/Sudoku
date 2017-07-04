b6b9cbf7abb2ad14faa09c9cffaea613894df12c
Ignacy Migas <gigi.migas@gmail.com>
2017-07-04 23:43:34 +0200

    Add ask window for sudoku image

--- Sudoku.py
+++ Sudoku.py
@@ -1,10 +1,15 @@
 import SudokuImageReader
 import Solver
+import os
+from Tkinter import Tk
+from tkFileDialog import askopenfilename
 
 
 if __name__ == "__main__":
-    image = "sudoku.jpg"
-    reader = SudokuImageReader.SudokuImageReader(image)
-    sudoku = Solver.Solver(reader.getBoard())
-    sudoku.solve()
-    sudoku.printSolve()
+    Tk().withdraw()
+    filename = askopenfilename(title="Open file", filetypes=[("JPG files", "*.jpg"), ("BMP files", "*.bmp")])
+    if os.path.isfile(filename):
+        reader = SudokuImageReader.SudokuImageReader(filename)
+        sudoku = Solver.Solver(reader.getBoard())
+        sudoku.solve()
+        sudoku.printSolve()
